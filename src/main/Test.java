1b6fe4156d84f872c893ab56dec3e7e2e78ba81b
kmtaegyu <134693467+kmtaegyu@users.noreply.github.com>
2023-05-26 18:51:37 +0900

    Add files via upload

---
+++ src/main/Test.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Test {
+	public static final int SCREEN_WIDTH = 1000;
+	public static final int SCREEN_HEIGHT = 640;
+	public static void main(String[] args) {
+		//RaceGameFrame t = new RaceGameFrame();
+		//game_screen sc = new game_screen();
+		Init sd = new Init();
+	}
+}
