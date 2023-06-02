b4b98bad3e95223627d954d55895e12b23007db0
eunhyekim811 <134694095+eunhyekim811@users.noreply.github.com>
2023-06-02 19:17:48 +0900

    Update Crong.java

--- src/main/Crong.java
+++ src/main/Crong.java
@@ -5,41 +5,45 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-
-public class Crong extends JLabel{
+public class Crong extends JLabel {
 	public final int SIZE = 200;
-	//위치상태
+	// 위치상태
 	private int x;
 	private int y;
 	private int x_right;
 	private int y_bottom;
-	
+
 	private boolean left;
 	private boolean right;
 	private boolean up;
 	private boolean down;
+	// private ImageIcon playerR1, playerR2, playerL1, playerL2,playerRdie,
+	// playerLdie;
 	
-	//private ImageIcon playerR1, playerR2, playerL1, playerL2,playerRdie, playerLdie;
-	
+	private boolean moving=false;
+
 	private final int SPEED = 4;
 	private final int JUMPSPEED = 2; // up, down
-	
-	private int state = 0; // 0 : live , 1 : die
-	private int life=3;
 
-	
+	private int state = 0; // 0 : live , 1 : die
+	private int life = 5;
+
 	public int getX_right() {
 		return x_right;
 	}
+
 	public int getY_bottom() {
 		return y_bottom;
 	}
+
 	public void setX_right(int x_right) {
 		this.x_right = x_right;
 	}
+
 	public void setHeight(int y_bottom) {
 		this.y_bottom = y_bottom;
 	}
+
 	public int getX() {
 		return x;
 	}
@@ -55,7 +59,6 @@ public class Crong extends JLabel{
 	public void setY(int y) {
 		this.y = y;
 	}
-
 
 	public boolean isLeft() {
 		return left;
@@ -96,16 +99,16 @@ public class Crong extends JLabel{
 	public void setState(int state) {
 		this.state = state;
 	}
-	
+
 	public Crong() {
 		initObject();
 		initSetting();
 	}
-	
-	private ImageIcon playerR1, playerR2, playerL1, playerL2,playerRdie, playerLdie;
-	
+
+	private ImageIcon playerR1, playerR2, playerL1, playerL2, playerRdie, playerLdie;
+
 	private void initObject() {
-		
+
 		playerL1 = new ImageIcon("crongL_1.png");
 		playerL2 = new ImageIcon("crongL_2.png");
 		playerR1 = new ImageIcon("crongR_1.png");
@@ -115,9 +118,9 @@ public class Crong extends JLabel{
 		Image imagel1 = playerL1.getImage();
 		Image imagel2 = playerL2.getImage();
 		Image newing1 = imager1.getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH);
-		Image newing2 = imager2.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		Image newing2 = imager2.getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH);
 		Image newing3 = imagel1.getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH);
-		Image newing4 = imagel1.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		Image newing4 = imagel2.getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH);
 		playerR1 = new ImageIcon(newing1);
 		playerR2 = new ImageIcon(newing2);
 		playerL1 = new ImageIcon(newing3);
@@ -127,11 +130,7 @@ public class Crong extends JLabel{
 		imagel1 = playerL1.getImage();
 		imagel2 = playerL2.getImage();
 	}
-	
-	
-	
 
-	
 	private void initSetting() {
 		x = 80;
 		y = 450;
@@ -142,25 +141,35 @@ public class Crong extends JLabel{
 		up = false;
 		down = false;
 //		life = 5;
-			
+
 		setIcon(playerR1);
 		setSize(SIZE, SIZE);
 		setLocation(x, y);
 	}
-	
+
 	public void loselife() {
-		life -= 0.5;
+		life -= 1;
 	}
-	
+
 	public void left() {
 		left = true;
-		new Thread(()-> {
-			while(left && getState() == 0) {
-				setIcon(playerL1);
-				//sleep(100);
-				//setIcon(playerL2);
-				if(x <= 0) {
-					x = 0;
+		new Thread(() -> {
+			while (left && getState() == 0) {
+//				setIcon(playerL1);
+				// sleep(100);
+				// setIcon(playerL2);
+				
+				if(moving==true) {
+					setIcon(playerL1);
+					moving=false;
+				}
+				else {
+					setIcon(playerL2);
+					moving=true;
+				}
+				
+				if (x <= -50) {
+					x = -50;
 				}
 				x = x - SPEED;
 				x_right = x + SIZE;
@@ -169,7 +178,7 @@ public class Crong extends JLabel{
 					Thread.sleep(10); // 0.01초
 				} catch (InterruptedException e) {
 					e.printStackTrace();
-				} 
+				}
 
 			}
 		}).start();
@@ -177,12 +186,20 @@ public class Crong extends JLabel{
 
 	public void right() {
 		right = true;
-		new Thread(()-> {
-			while(right && getState() == 0) {
-				setIcon(playerR1);
-				//setIcon(playerR2);
-				if(x >= 900) {
-					x = 900;
+		new Thread(() -> {
+			while (right && getState() == 0) {
+//				setIcon(playerR1);
+				// setIcon(playerR2);
+				if(moving==true) {
+					setIcon(playerR1);
+					moving=false;
+				}
+				else {
+					setIcon(playerR2);
+					moving=true;
+				}
+				if (x >= 850) {
+					x = 850;
 				}
 				x = x + SPEED;
 				x_right = x + SIZE;
@@ -191,13 +208,11 @@ public class Crong extends JLabel{
 					Thread.sleep(10); // 0.01초
 				} catch (InterruptedException e) {
 					e.printStackTrace();
-				} 
+				}
 			}
 		}).start();
 	}
-/*
-	// left + up, right + up
-	//@Override
+
 	public void up() {
 		//System.out.println("up");
 		up = true;
@@ -218,7 +233,6 @@ public class Crong extends JLabel{
 		}).start();
 	}
 
-	@Override
 	public void down() {
 		//System.out.println("down");
 		down = true;
@@ -226,18 +240,31 @@ public class Crong extends JLabel{
 			while(down) {
 				y = y + JUMPSPEED;
 				setLocation(x, y);
+				
+				if(y>=450) {
+					break;
+				}
 				try {
 					Thread.sleep(3);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-			down = false;
+			down=false;
 		}).start();
 	}
-	*/
+
 	public int getLife() {
-		// TODO Auto-generated method stub
 		return life;
+	}
+
+	public void setLife(int i) {
+		life = i;
+
+	}
+
+	public void pluslife() {
+		if (getLife() < 5)
+			life += 1;
 	}
 }
